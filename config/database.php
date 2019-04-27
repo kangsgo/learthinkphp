e5c3bf5b8d0caef256fa4b16bb2365a2c909d4fe
kangsgo <347657442@qq.com>
2019-04-27 23:28:15 +0800

    chart5

--- config/database.php
+++ config/database.php
@@ -15,13 +15,13 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => '',
+    'database'        => 'kangsgo_test',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'kangsgo_test',
     // 密码
-    'password'        => '',
+    'password'        => 'a123',
     // 端口
-    'hostport'        => '',
+    'hostport'        => '3306',
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
