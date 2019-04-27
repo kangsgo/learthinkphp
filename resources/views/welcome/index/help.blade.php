ef4698dd9956bc602a365583b8e044364b79e972
kangsgo <347657442@qq.com>
2019-04-27 22:02:15 +0800

    chart4

---
+++ resources/views/welcome/index/help.blade.php
@@ -0,0 +1,9 @@
+@extends('_layout.default')
+@section('title', '关于')
+@section('content')
+<section class="jumbotron text-center">
+    <div class="container">
+        <h1 class="jumbotron-heading">关于界面</h1>
+    </div>
+</section>
+@stop
